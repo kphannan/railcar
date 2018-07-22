3b54aef9ff016edae75d26640cf56294f82b921e
Kevin Hannan <kphannan@gmail.com>
2018-07-21 23:02:03 -0400

    Setup basic authentication - straight from a tutorial - quick and dirty way to have it work easily.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,14 @@
-package hello;
+// package hello;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
-public class HelloController {
+// @RestController
+// public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+//     @RequestMapping("/")
+//     public String index() {
+//         return "Greetings from Spring Boot!";
+//     }
 
-}
+// }
